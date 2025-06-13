db594595496b5e3396103d76ef2b9c44a77e2b41
Rui <116182303+Rui50@users.noreply.github.com>
2025-06-13 16:09:55 +0100

    timezone for backend

--- appFitness/src/main/java/com/example/appfitness/AppFitnessApplication.java
+++ appFitness/src/main/java/com/example/appfitness/AppFitnessApplication.java
@@ -1,13 +1,21 @@
 package com.example.appfitness;
 
+import java.util.TimeZone;
+
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppFitnessApplication {
 
+    @PostConstruct
+    void started() {
+        // Set the default time zone to Portugal
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Lisbon"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AppFitnessApplication.class, args);
     }
-
 }
